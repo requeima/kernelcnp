eee1a8633514e7ecd5bfac486adeeb6d6d70920f
James <james.requeima@gmail.com>
2021-03-16 11:50:03 -0500

    added plots to train

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import argparse
 import numpy as np
 import stheno.torch as stheno
 import torch
+import matplotlib.pyplot as plt
 
 import convcnp.data
 from convcnp.architectures import SimpleConv, UNet
@@ -67,6 +68,46 @@ def train(data, model, opt, report_freq):
         report_loss('Training', avg_loss, step, report_freq)
     return avg_loss
 
+# Move data to device
+to_numpy = lambda x: x.squeeze().cpu().numpy()
+
+def plot_task(task, model):
+    num_points = 200
+    x_all = torch.linspace(-2., 2., num_points)
+    
+    x_context = task['x_context'].to(device)
+    y_context = task['y_context'].to(device)
+
+    # Make predictions with model
+    with torch.no_grad():
+        y_mean, y_std = model(x_context, y_context, x_all[None, :, None].to(device))
+
+        # Get the marginals if we are predicting the full covariance
+        if y_std.shape[-1] > 1:
+            y_std = torch.diagonal(y_std, 0, dim1=-2, dim2=-1)[:, :, None]
+
+        # Plot context set
+        plt.scatter(to_numpy(x_context), to_numpy(y_context), label='Context Set', color='black')
+
+        # Plot model predictions
+        plt.plot(to_numpy(x_all), to_numpy(y_mean), label='Model Output', color='blue')
+        plt.fill_between(to_numpy(x_all),
+                         to_numpy(y_mean + 2 * y_std),
+                         to_numpy(y_mean - 2 * y_std),
+                         color='tab:blue', alpha=0.2)
+
+        # Make predictions with oracle GP (if GP dataset)
+        if args.data != 'sawtooth':
+            post = gp | (gp(to_numpy(x_context).astype(np.float64)), to_numpy(y_context).astype(np.float64))
+            gp_mean, gp_lower, gp_upper = post(to_numpy(x_all)).marginals()
+
+            plt.plot(to_numpy(x_all), gp_mean, color='black', label='Oracle GP')
+            plt.plot(to_numpy(x_all), gp_lower, color='black', alpha=0.8)
+            plt.plot(to_numpy(x_all), gp_upper, color='black', alpha=0.8)
+
+    plt.ylim(-3., 3)
+    plt.axis('off')
+    plt.legend(prop={'size': 16})
 
 # Parse arguments given to the script.
 parser = argparse.ArgumentParser()
@@ -122,6 +163,7 @@ if args.data == 'sawtooth':
     gen = convcnp.data.SawtoothGenerator()
     gen_val = convcnp.data.SawtoothGenerator(num_tasks=60)
     gen_test = convcnp.data.SawtoothGenerator(num_tasks=2048)
+    gen_plot = convcnp.data.SawtoothGenerator(num_tasks=16, batch_size=1, max_train_points=20)
 else:
     if args.data == 'eq':
         kernel = stheno.EQ().stretch(0.25)
@@ -139,6 +181,7 @@ else:
     gen = convcnp.data.GPGenerator(kernel=kernel)
     gen_val = convcnp.data.GPGenerator(kernel=kernel, num_tasks=60)
     gen_test = convcnp.data.GPGenerator(kernel=kernel, num_tasks=2048)
+    gen_plot = convcnp.data.GPGenerator(kernel=kernel, max_train_points=20, num_tasks=16, batch_size=1)
 
 # Load model.
 if args.model == 'convcnp':
@@ -201,9 +244,15 @@ else:
     load_dict = torch.load(wd.file('model_best.pth.tar', exists=True))
     model.load_state_dict(load_dict['state_dict'])
 
-# Finally, test model on ~2000 tasks.
+# Test model on ~2000 tasks.
 test_obj = validate(gen_test, model)
 print('Model averages a log-likelihood of %s on unseen tasks.' % test_obj)
 with open(wd.file('test_log_likelihood.txt'), 'w') as f:
     f.write(str(test_obj))
 
+# Plot the models
+for task_num, task in enumerate(gen_plot):
+    fig = plt.figure(figsize=(24, 8))
+    plot_task(task, model)
+    plt.savefig(wd.file('./%s/tmp_plot_%s' % (args.root, task_num), bbox_inches='tight'))
+    plt.close()
