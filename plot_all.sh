f95062fdb777d9bbe6af8a292bbd822170a8f600
Ubuntu <ubuntu@ip-172-31-68-71.ec2.internal>
2021-03-19 03:22:03 +0000

    fixed some bugs when running nonconv models

--- plot_all.sh
+++ plot_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-kernels=("eq" "matern" "noisy-mixture" "weakly-periodic" "sawtooth")
-# kernels=("matern" "noisy-mixture")
+# kernels=("eq" "matern" "noisy-mixture" "weakly-periodic" "sawtooth")
+kernels=("matern" "noisy-mixture")
 
 for data in "${kernels[@]}"; do
     python plot_all.py $data 
