2759cde1329cae052ee03a46eded200bbf324277
James <james.requeima@gmail.com>
2021-03-30 14:56:32 -0500

    Added argument logging to train file. Changed run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 MODE="--train"
-<<<<<<< Updated upstream
-EPOCHS="10000"
-=======
->>>>>>> Stashed changes
 kernels=("eq" "matern" "noisy-mixture" "weakly-periodic" "sawtooth")
 
 models=("GNP" "AGNP" "convGNP" "TEGNP")
@@ -14,12 +10,6 @@ basisdims=("4", "512")
 for data in "${kernels[@]}"; do
     for model in "${models[@]}"; do
         for cov in "${covs[@]}"; do
-<<<<<<< Updated upstream
-                python train.py $data $model $cov $MODE --epochs $EPOCHS --learning_rate 1e-4 --num_train_iters 5
-        done
-    done
-done
-=======
             for dim in "${basisdims[@]}"; do
                     python train.py $data $model $cov $MODE --num_basis_dim $dim
         done
@@ -33,4 +23,3 @@ for data in "${kernels[@]}"; do
             python train.py $data meanfield $cov $MODE
     done
 done
->>>>>>> Stashed changes
