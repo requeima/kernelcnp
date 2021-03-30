cab1e73d03f235aa15cc4145c13aacdb7db1f4ba
James <james.requeima@gmail.com>
2021-03-30 14:11:15 -0500

    run_all merge conflict

--- run_all.sh
+++ run_all.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
 MODE="--train"
+<<<<<<< Updated upstream
 EPOCHS="10000"
+=======
+>>>>>>> Stashed changes
 kernels=("eq" "matern" "noisy-mixture" "weakly-periodic" "sawtooth")
-# kernels=("eq")
 
 models=("GNP" "AGNP" "convGNP" "TEGNP")
-covs=("innerprod-homo" "innerprod-hetero" "kvv-homo" "kvv-hetero"  "meanfield")
+covs=("innerprod-homo" "innerprod-hetero" "kvv-homo" "kvv-hetero")
+basisdims=("4", "512")
 
 for data in "${kernels[@]}"; do
     for model in "${models[@]}"; do
         for cov in "${covs[@]}"; do
+<<<<<<< Updated upstream
                 python train.py $data $model $cov $MODE --epochs $EPOCHS --learning_rate 1e-4 --num_train_iters 5
         done
     done
 done
+=======
+            for dim in "${basisdims[@]}"; do
+                    python train.py $data $model $cov $MODE --num_basis_dim $dim
+        done
+    done
+done
+
+
+# meanfield
+for data in "${kernels[@]}"; do
+    for cov in "${covs[@]}"; do
+            python train.py $data meanfield $cov $MODE
+    done
+done
+>>>>>>> Stashed changes
