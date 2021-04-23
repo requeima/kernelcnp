518259351d42aef4df29cc2206b066dbde804b39
E. Markou <stratismar@gmail.com>
2021-04-23 09:51:30 +0000

    anp forward loss work, added halfunet

--- cnp/lnp.py
+++ cnp/lnp.py
@@ -40,18 +40,21 @@ class LatentNeuralProcess(nn.Module):
         r_dist = self.encoder(x_context, y_context, x_target)
         
         means = []
-        noise_scales = []
+        noise_vars = []
         
         for i in range(num_samples):
+            
             r = r_dist.rsample()
             z = self.decoder(r, x_context, y_context, x_target)
             
             means.append(z[..., :1])
-            noise_scales.append(self.add_noise(torch.zeros(size=(z.shape[0], z.shape[1], z.shape[0]),
-                                               z[..., 1:]))
+            noise_vars.append(self.add_noise(torch.zeros(size=(z.shape[0],
+                                                               z.shape[1],
+                                                               z.shape[1])),
+                                             z[..., 1:]))
             
         means = torch.stack(means, dim=0)
-        noise_scales = torch.stack(noises, dim=0)
+        noise_vars = torch.stack(noise_vars, dim=0)
         
         return means, noise_vars
 
@@ -64,16 +67,20 @@ class LatentNeuralProcess(nn.Module):
         D = y_target.shape[2]
         
         # Compute mean and variance tensors, each of shape (S, B, N, D)
-        means, noise_scales = self.forward(x_context,
-                                           y_context,
-                                           x_target,
-                                           num_samples)
+        means, noise_vars = self.forward(x_context,
+                                         y_context,
+                                         x_target,
+                                         num_samples)
+        
+        means = means[:, :, :, 0]
+        idx = torch.arange(noise_vars.shape[2])
+        noise_vars = noise_vars[:, :, idx, idx]
         
         # Normal with batch shape (S, B, N, D), event shape ()
-        normal = torch.distributions.Normal(loc=means, scales=noise_scales)
+        normal = torch.distributions.Normal(loc=means, scale=noise_vars ** 0.5)
         
         # Normal with batch shape (S), event shape (B, N, D)
-        normal = torch.distributions.Independent(normal, 3)
+        normal = torch.distributions.Independent(normal, 2)
         
         # Categorical to use for mixing components
         logits = torch.ones(size=(num_samples,))
@@ -81,7 +88,7 @@ class LatentNeuralProcess(nn.Module):
         
         mixture = torch.distributions.MixtureSameFamily(categorical, normal)
         
-        logprob = mixture.log_prob(y_target) / (B * N * D)
+        logprob = mixture.log_prob(y_target[:, :, 0]) / (B * N * D)
         
         return - logprob
     
@@ -109,16 +116,17 @@ class StandardANP(LatentNeuralProcess):
     
     def __init__(self, input_dim, add_noise):
         
-        # Standard input/output dimensions and latent representation dimension
+        # Standard input/output dim and latent representation dim
+        # latent_dim is common to stochastic and deterministic paths, and
+        # these are concatenated, producing a (2 * latent_dim) representation
         output_dim = 1
         latent_dim = 128
         
         # Decoder output dimension
-        decoder_output_dim = output_dim +               \
-                             add_noise.extra_noise_dim
+        decoder_output_dim = output_dim + add_noise.extra_noise_dim
 
         # Construct the standard encoder
-        encoder = StandardANPEncoder(input_dim=input_dim+output_dim,
+        encoder = StandardANPEncoder(input_dim=input_dim + output_dim,
                                      latent_dim=latent_dim)
         
         # Construct the standard decoder
