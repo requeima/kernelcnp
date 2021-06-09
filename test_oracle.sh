13f42d4c85f25fa9344de2d8e3c09c744f06fdc4
stratisMarkou <stratismar@gmail.com>
2021-06-09 17:01:58 +0100

    reshuffled files and dirs

--- test_oracle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kernels=("eq" "matern" "noisy-mixture" "noisy-mixture-slow" "weakly-periodic" "weakly-periodic-slow")
-
-for data in "${kernels[@]}"; do
-    python test_oracle.py $data 
-done
-
-
