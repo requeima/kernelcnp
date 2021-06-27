8303bcdc0a8ca9399d81a6630b9ae658c57de8b2
E. Markou <stratismar@gmail.com>
2021-06-27 16:25:20 +0100

    removed print line from cnp

--- cnp/cnp.py
+++ cnp/cnp.py
@@ -70,7 +70,6 @@ class GaussianNeuralProcess(nn.Module):
         y_cov = y_cov + jitter[None, :, :]
         
         diag = torch.diagonal(y_cov, dim1=1, dim2=2)
-        print('min, max', torch.min(diag), torch.max(diag))
 
         dist = MultivariateNormal(loc=y_mean[:, :, 0],
                                   covariance_matrix=y_cov)
