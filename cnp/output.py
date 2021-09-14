3b865ceb046b9a3b81a3588f0e280b3c787c212c
E. Markou <stratismar@gmail.com>
2021-09-14 15:03:37 +0100

    added more output layer functionality

--- cnp/output.py
+++ cnp/output.py
@@ -13,17 +13,13 @@ from abc import ABC, abstractmethod, abstractproperty
 class OutputLayer(nn.Module):
     
     
-    def __init__(self, noise_type):
+    def __init__(self):
         
         super().__init__()
-        
-        assert noise_type in ["homo", "hetero"]
-        
-        self.noise_type = noise_type
 
     
     @abstractmethod
-    def loss(self, tensor, y_target):
+    def loglik(self, tensor, y_target):
         """
         Implemented by child class.
         
@@ -82,31 +78,60 @@ class MeanFieldGaussianLayer(nn.Module):
     
     def __init__(self):
         
-        super().__init__(noise_type="hetero")
+        super().__init__()
+        
+        self.noise_unconstrained = nn.Parameter(torch.tensor(0.))
         
         
     def mean_and_cov(self, tensor):
         """
-        Computes mean and covariance of mean-field Gaussian layer.
+        Computes mean and covariance of mean-field Gaussian layer, as
+        specified by the parameters in *tensor*.
         
-        tensor : torch.tensor, (B, T, 2)
+        Arguments:
+            tensor : torch.tensor, (B, T, 2)
+            
+        Returns:
+            mean   : torch.tensor, (B, T)
+            cov    : torch.tensor, (B, T, T)
         """
         
         # Check tensor has three dimensions, and last dimension has size 2
         assert (len(tensor.shape) == 3) and (tensor.shape[2] == 2)
         
         # Mean is the first feature of the tensor
-        mean = tensor[:, :, :1]
+        mean = tensor[:, :, 0]
         
         # Covariance is a matrix with a 
-        cov = torch.nn.Softplus()(tensor[:, :, 1:])
-        cov = 
+        f_var = torch.nn.Softplus()(tensor[:, :, 1])
+        y_var = f_var + torch.nn.Softplus()(self.noise_unconstrained)
         
-        return 
+        f_cov = torch.diag_embed(f_var)
+        y_cov = torch.diag_embed(y_var)
+        
+        return mean, f_cov, y_cov
         
     
-    def loss(self, tensor, y_target):
-        pass
+    def loglik(self, tensor, y_target):
+        
+        # Compute mean and covariance from feature vector
+        y_mean, _, y_cov = self.mean_and_cov(tensor)
+
+        y_mean = mean.double()
+        y_cov = cov.double()
+        y_target = y_target.double()
+
+        jitter = 1e-6 * torch.eye(y_cov.shape[-1], device=y_cov.device)
+        jitter = jitter[None, :, :].double()
+        
+        cov = cov + jitter
+        
+        dist = MultivariateNormal(loc=mean[:, :, 0],
+                                  covariance_matrix=cov)
+        nll = - torch.mean(dist.log_prob(y_target[:, :, 0]))
+
+        return nll.float()
+    
 
     
     def sample(self, tensor, num_samples):
@@ -137,10 +162,8 @@ class InnerprodGaussianLayer(nn.Module):
         tensor : torch.tensor, (B, T, D)
         """
         
-        
 
-    
-    def loss(self, tensor, y_target):
+    def loglik(self, tensor, y_target):
         pass
 
     
@@ -165,7 +188,7 @@ class KvvGaussianLayer(nn.Module):
         super().__init__()
 
     
-    def loss(self, tensor, y_target):
+    def loglik(self, tensor, y_target):
         pass
 
     
@@ -190,7 +213,7 @@ class GammaGaussianCopulaLayer(nn.Module):
         super().__init__()
 
     
-    def loss(self, tensor, y_target):
+    def loglik(self, tensor, y_target):
         pass
 
     
